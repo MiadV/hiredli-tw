161f456e4ad304b053b3550507fbbcc5ab7a5d1b
miadv <miadsoft@gmail.com>
2022-02-22 18:40:04 +0800

    refactor: remove layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,32 +6,15 @@ import 'swiper/css/scrollbar';
 import 'swiper/css/navigation';
 //
 import '@/css/main.css';
-import { Fragment } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { Header } from '@/components/Header';
-import { LayoutProps } from '@/types';
-import { TitleMeta } from '@/components/TitleMeta';
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{ layoutProps: LayoutProps }>) {
-  const layoutProps = Component.defaultProps?.layoutProps;
-  const Layout = (layoutProps && layoutProps.Layout) || Fragment;
-
-  const layoutPropsObj =
-    layoutProps && layoutProps.Layout ? { layoutProps: layoutProps } : {};
-
-  const meta = layoutProps && layoutProps.meta;
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
-      <TitleMeta suffix="Hiredli">{meta?.title}</TitleMeta>
+    <ThemeProvider attribute='class' defaultTheme='system'>
       <Header />
-      <Layout {...layoutPropsObj}>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
